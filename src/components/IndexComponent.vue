f0824171b6a3bb672ba68202d0997334d6e1329a
a.epova <epova.aa@gmail.com>
2023-04-24 16:42:25 +0300

    [~] with chatGPT(2)

--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -4,24 +4,27 @@
 
     <form id="city" @submit.prevent="submitForm">
 
-        <div v-if="!selectedPoint">
+        <div >
             <label>Введите название города:</label>
             <input v-model="city" type="text" placeholder="Название города">
-            <button @click.prevent="showMap()">Найти пункты выдачи</button>
+            <button @click.prevent="fetchPvzList()">Найти пункты выдачи</button>
         </div>
 
         <div class="map" id="map"></div>
 
 
-<!--        <div class="select">
-            <input type="hidden" :value="this.post">
-            <div class="select__header">{{this.post}}</div>
+        <div class="select">
+<!--            <input type="hidden" :value="this.post">-->
+<!--            <div class="select__header">{{this.post}}</div>
             <ul class="select__list">
                 <li class="select__list-item" v-for="postItem in postsList" @click="event => this.post = event.target.innerText">
                     {{postItem.address}}
                 </li>
-            </ul>
-        </div>-->
+            </ul>-->
+            <button class="select__map" @click.prevent="showMap()">
+                Показать на карте
+            </button>
+        </div>
 
 <!--        <div v-if="selectedPoint">
             <button class="button-map" @click.prevent="selectedPoint = !selectedPoint">Назад</button>
@@ -57,58 +60,131 @@ export default {
         return {
             city: '',
             pvzList: [],
-
+            point: [],
         }
     },
 
     methods: {
         async fetchPvzList() {
-            /*//получаем данные о введенном городе
+            //получаем данные о введенном городе
             const geocodeUrl = `https://geocode-maps.yandex.ru/1.x/?apikey=${yandexApiKey}&format=json&geocode=${this.city}`;
             const geocodeResponse = await fetch(geocodeUrl);
             const geocodeData = await geocodeResponse.json();
 
-            const point = geocodeData.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ');
-            const [longitude, latitude] = point;
-
-            // Здесь должны получать список ПВЗ
-            if (longitude && latitude) {
-                const searchUrl = `https://search-maps.yandex.ru/v1/?apikey=${yandexApiKey}&text=cdek&lang=ru_RU&ll=${longitude},${latitude}&type=biz&results=10`;
-                const searchData = await fetch(searchUrl)
-                    .then((data) => data.json())
-                    .catch(error => {
-                        throw(error);
-                    });
-
-                if (searchData) {
-                    this.pvzList = searchData.features.filter(feature => feature.properties.CompanyMetaData.name === "СДЭК Пункт выдачи заказов");
-                }
-            }*/
+            let point = geocodeData.response.GeoObjectCollection.featureMember[0].GeoObject.boundedBy.Envelope;
+            for (let pointItem in point) {
+                let part = [];
+                part.push(point[pointItem].split(" ").reverse());
+                this.point.push(part);
+            }
         },
 
         async showMap() {
+            let cityCoords = [];
 
-            const apiKey = '88398772-1a4b-4234-b8b9-b3dacf1b135e'; // Вставьте свой API-ключ
-            const city = 'Moscow'; // Название города, для которого необходимо получить список ПВЗ
+            cityCoords = Array.from(this.point).map(proxyArray => {
 
-            fetch(`https://search-maps.yandex.ru/v1/?text=Пункт выдачи заказов СДЭК ${city}&type=biz&apikey=${apiKey}`)
-                .then(response => {
-                    console.log(response)
-                    response.json()
-                })
-                /*.then(data => {
-                    const results = data.features.map(feature => {
-                        return {
-                            name: feature.properties.name,
-                            address: feature.properties.description,
-                            coords: feature.geometry.coordinates
-                        };
-                    });
-                    console.log(results); // Список ПВЗ
-                })*/
-                .catch(error => {
-                    console.error(error);
+                console.log(proxyArray)
+
+                Object.values(proxyArray)
+            });
+
+            console.log(cityCoords)
+
+            ymaps.ready(function() {
+
+                const myMap = new ymaps.Map("map", {
+                    center: cityCoords,
+                    zoom: 10
                 });
+
+                // это просто для примера
+                /*const myGeocoder = ymaps.geocode(`СДЕК ПВЗ Москва`, {results: 20})
+                    .then(res => {
+                        const geoObjects = res.geoObjects.toArray();
+
+                        geoObjects.forEach(geoObjectsItem => {
+                            const coords = geoObjectsItem.geometry.getCoordinates();
+                            const objectData = geoObjectsItem.properties.getAll()
+
+                            console.log('Тип геообъекта: %s', geoObjectsItem.properties.get('metaDataProperty.GeocoderMetaData.kind'));
+
+                            // создание балуна
+                            const myPlacemark = new ymaps.Placemark(coords, {
+                                balloonContentHeader: "Тут будет название пункта",
+                                balloonContentBody: "" +
+                                    "Тут описание" +
+                                    "<ul>" +
+                                    "<li>время работы;</li>" +
+                                    `<li>${objectData.text}</li>` +
+                                    "</ul>",
+                                balloonContentFooter: "<button>Выбрать этот пункт</button>",
+                                hintContent: "улица такая-то"
+                            }, {
+                                preset: 'islands#violetStretchyIcon'
+                            });
+
+                            myMap.geoObjects.add(myPlacemark);
+                        });
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                    });*/
+
+                // подключаем элемент управления "Поиск по карте"
+                var searchControl = new ymaps.control.SearchControl({
+                    options: {
+                        provider: 'yandex#search',
+                        noPopup: true,
+                        strictBounds: false,
+                    }
+                });
+
+                myMap.controls.add(searchControl);
+
+                // это мы запускаем поиск и отображение точек на карте
+                searchControl.search('СДЕК ПВЗ Москва');
+
+
+                //это мы должны получать данные поиска, но что-то пошло не так
+                searchControl.events.add('searchComplete', function (event) {
+
+                    console.log("search Complete");
+
+                    /*var results = searchControl.getResultsArray();
+
+                    // Создаем массив для хранения найденных меток
+                    var placemarks = [];
+
+                    // Проходимся по всем результатам поиска
+                    for (var i = 0; i < results.length; i++) {
+                        var result = results[i];
+                        var geoObject = result.geoObject;
+
+                        // Если объект - метка, то добавляем его в массив меток
+                        if (geoObject.properties.get('iconCaption') === 'Метка') {
+                            placemarks.push({
+                                name: geoObject.properties.get('name'),
+                                address: geoObject.properties.get('description'),
+                                coords: geoObject.geometry.getCoordinates()
+                            });
+                        }
+                    }
+
+                    console.log(placemarks);*/
+                });
+
+
+                // это мы получаем выбранный элемент (на который тыкнул пользователь)
+                searchControl.events.add("resultselect", e => {
+
+                    searchControl.getResultsArray().forEach(item => {
+                        const data = item.properties.getAll();
+                    })
+
+                })
+            });
+
         },
 
         hideMap() {
