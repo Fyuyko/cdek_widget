3309636d387f369028a77990b163a1f981829c1e
a.epova <epova.aa@gmail.com>
2023-04-28 11:48:14 +0300

    [~] merge

---
+++ src/components/mapComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="isMapActive" class="map" id="map"></div>
+</template>
+
+<script>
+export default {
+    name: "mapComponent",
+    props: ["isMapActive"],
+}
+
+</script>
+
+<style lang="scss">
+  #map {
+    width: 600px;
+    height: 400px;
+    margin: 0 auto;
+
+    padding-top: 40px;
+
+    &.show {
+      opacity: 1;
+      pointer-events: all;
+    }
+  }
+</style>
