a77eeabd0751b577efd9b23936e318460d846121
a.epova <epova.aa@gmail.com>
2023-04-28 16:29:59 +0300

    [~] start correcting app: - combine app in one file - start create app structure

--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -1,21 +1,46 @@
 <template>
-    <h1>Выбор ПВЗ СДЭК</h1>
-    <form class="form">
-      <TemplateComponent/>
-    </form>
+    <div class="delivery-point__fade">
+        <div class="delivery-point">
+            <h1>Выбор ПВЗ СДЭК</h1>
+            <form class="form">
+                <TemplateComponent/>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
 import TemplateComponent from "@/components/TemplateComponent.vue";
-import CourierDelivery from "@/components/CourierDelivery/CourierDeliveryComponent.vue";
 
 export default {
     name: "IndexComponent",
-    components: {CourierDelivery, TemplateComponent},
+    components: {TemplateComponent},
 }
 
 </script>
 
 <style scoped lang="scss">
+  .delivery-point__fade {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.4);
+    z-index: 1000;
 
+    transition: .2s all linear .2s;
+  }
+
+  .delivery-point {
+    background-color: white;
+    max-width: 80%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 30px 50px;
+
+    transition: .2s all linear .2s;
+  }
 </style>
--- src/components/TemplateComponent.vue
+++ src/components/TemplateComponent.vue
@@ -1,38 +1,116 @@
 <template>
     <div class="template">
-        <CityComponent :isMapActive="isMapActive" :isSelect="isSelect" @onGetSelectItem="getSelectedItem" @onUpdateIsSelect="updateIsSelect" @onUpdateIsMapActive="updateIsMapActive"/>
-        <MapComponent :isMapActive="isMapActive"/>
-        <SelectItemComponent :isSelect="isSelect" :selectedItem="selectedItem" @onUpdateIsSelect="updateIsSelect" @onUpdateIsMapActive="updateIsMapActive"/>
+        <div class="city-delivery-point">
+            <div v-if="!isMapActive && !isSelect">
+                <label>Введите название города:</label>
+                <input v-model="city" type="text" placeholder="Название города">
+                <button @click.prevent="mapHandler">Показать пункты выдачи на карте</button>
+            </div>
+            <div v-else-if="isMapActive">
+                <button @click.prevent="difCity">Выбрать другой город</button>
+            </div>
+        </div>
+
+        <div class="map-delivery-point">
+            <div v-if="isMapActive" class="map" id="map"></div>
+        </div>
+
+        <div class="select-delivery-point">
+            <button v-if="!IsMapActive && isSelect">
+                Выбрать другой пункт
+            </button>
+            <div v-if="selectedItem && !isSelect">
+                <button @click.prevent="submitForm()" type="submit">Выбрать этот пункт</button>
+            </div>
+            <div v-else-if="selectedItem && isSelect">
+                Вы выбрали пункт по улице {{itemAddress}}
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import CityComponent from "@/components/CityComponent.vue";
-import MapComponent from "@/components/MapComponent.vue";
-import SelectItemComponent from "@/components/SelectItemComponent.vue";
+const yandexApiKey = "88398772-1a4b-4234-b8b9-b3dacf1b135e";
 
 export default {
     name: "TemplateComponent",
-    components: {SelectItemComponent, MapComponent, CityComponent},
 
     data() {
         return {
-            change: false,
-            selectedItem: null,
-            isSelect: null,
-            isMapActive: null,
+            city: "",
+            selectedItem: false,
+            isMapActive: false,
+            isSelect: false,
+
+            //pvzList: [],  Можно использовать для выведения селекта
+            itemAddress: "",
         }
     },
 
     methods: {
-        getSelectedItem(data) {
-            this.selectedItem = data;
+        async mapHandler() {
+            this.isMapActive = true;
+
+            const geocodeData = await fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=${yandexApiKey}&format=json&geocode=${this.city}`).then(res => res.json()); // получаем данные о городе
+            const cityLimits = geocodeData.response.GeoObjectCollection.featureMember[0].GeoObject.boundedBy.Envelope;  // Границы полученного города
+            const cityCenter = geocodeData.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(" ").reverse();  //Центр города
+            let cityLimitsArray = [cityLimits.lowerCorner.split(" ").reverse(), cityLimits.upperCorner.split(" ").reverse()];
+
+            let selectedItem;
+            let changeSelectedItem = newSelectedItem => {
+                selectedItem = newSelectedItem;
+                this.selectedItem = selectedItem;
+            }
+
+            ymaps.ready(() => {
+                let myMap;
+
+                myMap = new ymaps.Map("map", {
+                    center: cityCenter,
+                    zoom: 13
+                });
+
+                // подключаем элемент управления "Поиск по карте"
+                let searchControl = new ymaps.control.SearchControl({
+                    options: {
+                        provider: 'yandex#search',
+                        results: 100,
+                        noPopup: true,
+                        strictBounds: false,
+                        boundedBy: cityLimitsArray,
+                    }
+                });
+
+                // это мы запускаем поиск и отображаем точки на карте
+                searchControl.search(`СДЕК ПВЗ ${this.city}`).then(res => {
+                    map.geoObjects.add(res.geoObjects);
+                });
+
+                // это мы получаем выбранный элемент (на который тыкнул пользователь)
+                searchControl.events.add("resultselect", e => {
+                    searchControl.getResult(e.get('index')).then(res => {
+                        let selectedItem = res.properties.getAll();
+                        changeSelectedItem(selectedItem);
+                    });
+                })
+
+                myMap.controls.add(searchControl);
+                myMap.container.fitToViewport();
+            });
         },
-        updateIsSelect(newValue) {
-            this.isSelect = newValue;
+        difCity() {
+            this.isMapActive = false;
+            this.selectedItem = null;
+            this.isSelect = null;
         },
-        updateIsMapActive(data) {
-            this.isMapActive = data;
+
+        submitForm() {
+            const selectedPoint = this.selectedItem;
+            this.itemAddress = this.selectedItem.address;
+            this.isMapActive = false;
+            this.isSelect = true;
+
+            console.log(selectedPoint)
         },
     }
 }
@@ -41,5 +119,17 @@ export default {
 <style lang="scss">
     .template {
         text-align: center;
+    }
+    #map {
+        width: 600px;
+        height: 400px;
+        margin: 0 auto;
+
+        padding-top: 40px;
+
+        &.show {
+            opacity: 1;
+            pointer-events: all;
+        }
     }
 </style>
